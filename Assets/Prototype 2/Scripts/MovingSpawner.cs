5412a9bef902bc7e0869deeb837cc1b075671968
FreakyPerfectCell <Mlizardi1225@gmail.com>
2025-09-10 22:22:03 -0400

    prototype 2

---
+++ Assets/Prototype 2/Scripts/MovingSpawner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace PrototypeTwo
+{
+    public class MovingSpawner : MonoBehaviour
+    {
+        public float speed;
+        public int startingPoint;
+        public Transform[] points;
+
+        public int i;
+
+        void Start()
+        {
+            transform.position = points[startingPoint].position;
+        }
+
+        void Update()
+        {
+            if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+            {
+                i++;
+                if (i == points.Length)
+                {
+                    i = 0;
+                }
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+        }
+    }
+}
